184d1fee844544b014185c7971b3601fa6fedec1
eneserdo1 <eneserdogan.66@outlook.com>
2020-11-19 17:14:30 +0300

    New Pattern

--- app/src/main/java/com/eneserdogan/keovecasekotlin/View/MainActivity.kt
+++ app/src/main/java/com/eneserdogan/keovecasekotlin/View/MainActivity.kt
@@ -4,44 +4,58 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.eneserdogan.keovecasekotlin.R
 import com.eneserdogan.keovecasekotlin.model.User
 import com.eneserdogan.keovecasekotlin.AuthorizationServices.RestApi
+import com.eneserdogan.keovecasekotlin.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        
         btnLogin.setOnClickListener {
-
             /* Edittextlerin doluluk kontrolü yapılıyor*/
-            if(userNameEt.text.toString() =="" || passwordEt.text.toString() ==""){
-                    Toast.makeText(applicationContext,"Gerekli Alanları Doldurunuz!",Toast.LENGTH_LONG).show()
-            }else{
-                signin()
+            if (userNameEt.text.toString() == "" || passwordEt.text.toString() == "") {
+                Toast.makeText(
+                    applicationContext,
+                    "Gerekli Alanları Doldurunuz!",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                val userInfo = User(
+                    username = userNameEt.text.toString(),
+                    password = passwordEt.text.toString().toInt()
+                )
+                viewModel.refreshData(userInfo)
             }
-
-
+            observeData()
         }
     }
-    fun signin() {
-        /*RestApi sınıfı üzerinden post işlemi yapılıyor*/
-        val apiService = RestApi()
-        val userInfo = User(username = userNameEt.text.toString(),password = passwordEt.text.toString().toInt())
 
-        apiService.getInfo(userInfo){loginInfo ->
-            loginInfo?.let {
-                println("loginn "+loginInfo.refreshToken)
-                /*refreshToken ve jwtToken Diğer Intent'e gönderiliyor*/
-                val intent=Intent(applicationContext,WeatherActivity::class.java)
-                intent.putExtra("jwtToken",loginInfo.jwtToken)
-                intent.putExtra("refreshToken",loginInfo.refreshToken)
+
+    private fun observeData() {
+        viewModel.data.observe(this, Observer { info ->
+            info?.let {
+                val intent = Intent(applicationContext, WeatherActivity::class.java)
+                intent.putExtra("jwtToken", info.jwtToken)
+                intent.putExtra("refreshToken", info.refreshToken)
+                Toast.makeText(applicationContext,"Giriş Başarılı", Toast.LENGTH_LONG).show()
                 startActivity(intent)
-                Toast.makeText(applicationContext,"Giriş Başarılı",Toast.LENGTH_LONG).show()
-            }
 
-        }
+            }
+        })
+
     }
+
 }
--- app/src/main/java/com/eneserdogan/keovecasekotlin/View/WeatherActivity.kt
+++ app/src/main/java/com/eneserdogan/keovecasekotlin/View/WeatherActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.eneserdogan.keovecasekotlin.LogOutServices.RestApiLogout
@@ -14,6 +16,7 @@ import com.eneserdogan.keovecasekotlin.RefreshTokenServices.RestApiRefresh
 import com.eneserdogan.keovecasekotlin.adapter.WeatherAdapter
 import com.eneserdogan.keovecasekotlin.model.RefreshModel
 import com.eneserdogan.keovecasekotlin.model.Weather
+import com.eneserdogan.keovecasekotlin.viewmodel.WeatherViewModel
 import com.eneserdogan.keovecasekotlin.weatherServices.WeatherAPIService
 import kotlinx.android.synthetic.main.activity_weather.*
 import kotlinx.coroutines.launch
@@ -26,10 +29,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 class WeatherActivity : AppCompatActivity() {
 
     private val recyclerAdapter = WeatherAdapter(arrayListOf())
+    private lateinit var viewModel: WeatherViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
+
+        viewModel = ViewModelProviders.of(this).get(WeatherViewModel::class.java)
 
         /*MainAcitivity'den gelen değerler alınıyor*/
         val jwtToken = intent.getStringExtra("jwtToken")
@@ -39,74 +45,48 @@ class WeatherActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
         recyclerView.adapter = recyclerAdapter
 
+        viewModel.getWeatherData(jwtToken.toString())
+
         /*Timer ile 5 dk geri sayım başlıyor*/
         val timer = object : CountDownTimer(300000, 1000) {
             override fun onFinish() {
-                timeToRefresh(refreshToken.toString())
+                viewModel.timeToRefresh(refreshToken.toString())
             }
             override fun onTick(p0: Long) {
                 println("Geri sayım başladı")
             }
         }
         timer.start()
+        //Veriler Observe Ediliyor
+        observeData()
 
         /*Log out durumu dinlenerek tıklanıldığında RestApiLogout üzerinden işlemler başlatılıyor */
         btnLogOut.setOnClickListener {
-            val restApi=RestApiLogout()
-            val refreshModel=RefreshModel(refreshToken)
-            restApi.invokeToken(refreshModel,jwtToken.toString()){message ->
-                message?.let {
-                    Toast.makeText(applicationContext,"Çıkış Yapıldı",Toast.LENGTH_LONG).show()
-                    val intent=Intent(applicationContext,MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                }
-            }
-        }
-
-        /*Hava durumu verileri çekilerek adaptera set ediliyor*/
-        lifecycleScope.launch {
-            val api = Retrofit.Builder()
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl("http://testcase1.keove.com/")
-                .build()
-
-            val getServis = api.create(WeatherAPIService::class.java)
-            val myCall = getServis.getData("Bearer " + jwtToken)
-            myCall.enqueue(object : Callback<List<Weather>> {
-                override fun onResponse(
-                    call: Call<List<Weather>>,
-                    response: Response<List<Weather>>
-                ) {
-                    val weathers = response.body()!!
-                    println("Weather response girdi")
-                    recyclerAdapter.updateList(weathers)
-                }
-
-                override fun onFailure(call: Call<List<Weather>>, t: Throwable) {
-                    Log.e("ERROR", t.message.toString())
-                    Toast.makeText(applicationContext, "Cevap Gelmedi", Toast.LENGTH_LONG)
-                        .show()
-                }
-            })
+            viewModel.LogOut(refreshToken.toString(), jwtToken.toString())
+            val intent=Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
         }
     }
 
-    /*5 dk dolunca bu fonksiyon çalışarak yeni jwtToken alınır*/
-    fun timeToRefresh(refreshToken: String) {
-        println("gelen refreshToken" + refreshToken)
-
-        val restApi = RestApiRefresh()
-        val token = RefreshModel(refreshToken)
-        restApi.getnewToken(token) { newData ->
-            newData?.let {
-                println("Girdi")
-                println("yeni jwt token " + it.jwtToken)
-                println("yeni refreshtoken " + it.refreshToken)
+    private fun observeData() {
+        viewModel.weathers.observe(this, Observer { data ->
+            data?.let {
+                recyclerAdapter.updateList(data)
             }
-        }
-    }
+        })
+        viewModel.infoData.observe(this, Observer { data ->
+            data?.let {
+                Toast.makeText(applicationContext, "İnfo Observe Edildi", Toast.LENGTH_LONG).show()
+            }
+        })
 
+        viewModel.logOutData.observe(this, Observer { message ->
+            message?.let {
+                Toast.makeText(applicationContext, "Çıkış Yapıldı ${it.message}", Toast.LENGTH_LONG).show()
+                println("viewmodel logout")
+            }
+        })
+    }
 
 
 }
